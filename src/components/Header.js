966fc5ad0b2aad0689ed58bb75bfd54488b4480f
Bryan Hamilton <3054666@ads.qub.ac.uk>
2022-11-11 16:11:23 +0000

    navbar collapse

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,22 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from "react-router-dom";
-import { GrFavorite } from "react-icons/gr";
 
 function Header({profiles, planets, starships}) {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" variant="dark" expand="md">
         <Container>
-          <Navbar.Brand>
-            Star Wars API
-          </Navbar.Brand>
-          <Nav>
+          <Navbar.Brand>Star Wars API</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navigation" />
+          <Navbar.Collapse id="navigation">
+          <Nav id="navBar">
             <Link to="/" >Home</Link>
             <Link to="/planet-listing">Planets</Link>
             <Link to="/starship-listing">Starships</Link>
             <Link to="/favourites">Favourites</Link>
         </Nav>
+        </Navbar.Collapse> 
         </Container>
       </Navbar>
     </>
